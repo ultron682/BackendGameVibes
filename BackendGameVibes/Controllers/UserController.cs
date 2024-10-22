8ececb552d8352d77c3f57b672f5b1391b88654a
Michał Mazur <52131708+ultron682@users.noreply.github.com>
2024-10-22 19:15:38 +0200

    Add userController for info about one player

---
+++ BackendGameVibes/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using BackendGameVibes.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendGameVibes.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase {
+        private readonly IAccountService _accountService;
+
+        public UserController(IAccountService accountService) {
+            _accountService = accountService;
+        }
+
+        [HttpGet(":id")]
+        public async Task<ActionResult<object>> GetUser(string id) {
+            var user = await _accountService.GetUserByIdAsync(id);
+            if (user == null) {
+                return NotFound();
+            }
+            else {
+                var accountInfo = await _accountService.GetAccountInfoAsync(user.Id, user);
+                return Ok(accountInfo);
+            }
+        }
+
+
+
+    }
+}
