8007ffa51ebb02d541332ba9bc1f836205a2aff8
Michał Mazur <52131708+ultron682@users.noreply.github.com>
2024-10-25 00:42:27 +0200

    Add to UserController template endpoints send-friend-request, confirm-friend-request

--- BackendGameVibes/Controllers/UserController.cs
+++ BackendGameVibes/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using BackendGameVibes.IServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace BackendGameVibes.Controllers {
     [Route("api/[controller]")]
@@ -13,7 +14,7 @@ namespace BackendGameVibes.Controllers {
         }
 
         [HttpGet(":id")]
-        public async Task<ActionResult<object>> GetUser(string id) {
+        public async Task<ActionResult<object>> GetUserAsync(string id) {
             var user = await _accountService.GetUserByIdAsync(id);
             if (user == null) {
                 return NotFound();
@@ -22,6 +23,33 @@ namespace BackendGameVibes.Controllers {
                 var accountInfo = await _accountService.GetAccountInfoAsync(user.Id, user);
                 return Ok(accountInfo);
             }
+        }
+
+        [HttpPost("send-friend-request:id")]
+        public async Task<ActionResult<object>> SendFriendRequstAsync([FromQuery] string id) {
+            var user = await _accountService.GetUserByIdAsync(id);
+            if (user == null) {
+                return NotFound();
+            }
+            else {
+
+                return Ok("ToDo");
+            }
+        }
+
+        [HttpPost("confirm-friend-request:id")]
+        [SwaggerOperation("todo: token bedzie generowany przez wewnętrzny mechanizm i wysylany na email. uzytkownik klikajac na link w emailu odpala ten link")]
+        public async Task<ActionResult<object>> ConfirmFriendRequstAsync(string token) {
+            //var user = await _accountService.GetUserByIdAsync(id);
+            //if (user == null) {
+            //    return NotFound();
+            //}
+            //else {
+
+            //    return Ok("ToDo");
+            //}
+
+            return Ok("Todo");
         }
     }
 }
