337b11db254cd321dcfd5650c57df373b6d1a0ef
Michał Mazur <52131708+ultron682@users.noreply.github.com>
2024-10-29 22:05:41 +0100

    Add ReportedPost, ReportedReview models

---
+++ BackendGameVibes/Models/Requests/ReportPostDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace BackendGameVibes.Models.Requests {
+    public class ReportPostDTO {
+        public string? Reason { get; set; }
+        public int PostId { get; set; }
+        public string? ReporterUserId { get; set; }
+    }
+}
