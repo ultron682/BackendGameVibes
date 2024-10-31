9721c9021475fbf19669a04152cfc925ba758df4
Michał Mazur <52131708+ultron682@users.noreply.github.com>
2024-10-31 23:58:39 +0100

    Add AddNewFriendPoints method to IForumExperienceService

--- BackendGameVibes/IServices/IForumExperienceService.cs
+++ BackendGameVibes/IServices/IForumExperienceService.cs
@@ -2,6 +2,7 @@
 {
     public interface IForumExperienceService
     {
+        Task<int?> AddNewFriendPoints(string userId);
         Task<int?> AddPostPoints(string userId);
         Task<int?> AddReviewPoints(string userId);
         Task<int?> AddThreadPoints(string userId);
