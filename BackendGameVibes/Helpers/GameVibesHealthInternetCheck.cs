f45d17e785e7513811d185fb618e960a02303e0f
Michał Mazur <52131708+ultron682@users.noreply.github.com>
2024-11-17 23:07:39 +0100

    Add GameVibes Health Internet Check

---
+++ BackendGameVibes/Helpers/GameVibesHealthInternetCheck.cs
@@ -0,0 +1,26 @@
+﻿using BackendGameVibes.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Data.Common;
+
+namespace BackendGameVibes.Helpers {
+    public class GameVibesHealthInternetCheck : IHealthCheck {
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default) {
+            try {
+                using (var httpClient = new HttpClient()) {
+                    var response = await httpClient.GetAsync("https://1.1.1.1", cancellationToken);
+                    if (response.IsSuccessStatusCode) {
+                        return HealthCheckResult.Healthy("Internet connection is healthy");
+                    }
+                    else {
+                        return HealthCheckResult.Unhealthy("Internet connection failed");
+                    }
+                }
+            }
+            catch (HttpRequestException ex) {
+                return HealthCheckResult.Unhealthy("Internet connection failed", ex);
+            }
+        }
+    }
+}
