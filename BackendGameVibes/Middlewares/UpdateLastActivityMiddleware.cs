7b8bd93d0532d2354eb1fb2e4dad53cd86f7efec
Michał Mazur <52131708+ultron682@users.noreply.github.com>
2024-11-22 13:30:36 +0100

    Add middleware for auto update lastactivitydate

---
+++ BackendGameVibes/Middlewares/UpdateLastActivityMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using BackendGameVibes.Models.User;
+using Microsoft.AspNetCore.Identity;
+
+namespace BackendGameVibes.Middlewares {
+    public class UpdateLastActivityMiddleware {
+        private readonly RequestDelegate _next;
+
+        public UpdateLastActivityMiddleware(RequestDelegate next) {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, UserManager<UserGameVibes> userManager) {
+            if (context.User.Identity is not null && context.User.Identity.IsAuthenticated) {
+                var user = await userManager.GetUserAsync(context.User);
+                if (user != null) {
+                    user.LastActivityDate = DateTime.Now;
+                    await userManager.UpdateAsync(user);
+                }
+            }
+
+            await _next(context);
+        }
+    }
+
+}
