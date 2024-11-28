53658bd055bfc3d77eaada598e19f1044d0fd808
Michał Mazur <52131708+ultron682@users.noreply.github.com>
2024-11-28 20:38:20 +0100

    Add GetAllReviewsResponse model

---
+++ BackendGameVibes/Models/DTOs/Responses/GetAllReviewsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendGameVibes.Models.DTOs.Responses;
+
+public class GetAllReviewsResponse {
+    public int TotalResults { get; set; }
+    public int PageSize { get; set; }
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public object[]? Data { get; set; }
+}
