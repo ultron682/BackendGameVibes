c5099eedaba7aede1bd23e4916b77bc6594b8261
Michał Mazur <52131708+ultron682@users.noreply.github.com>
2024-12-02 22:19:45 +0100

    Add IntegrationTestBase with configured DB In memeory

---
+++ BackendGameVibes.Tests/IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,33 @@
+﻿namespace BackendGameVibes.Tests.IntegrationTests;
+
+using BackendGameVibes.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Program = BackendGameVibes.Program;
+
+
+public class IntegrationTestBase : IClassFixture<WebApplicationFactory<Program>> {
+    protected readonly WebApplicationFactory<Program> _factory;
+    protected readonly HttpClient _client;
+
+    public IntegrationTestBase(WebApplicationFactory<Program> factory) {
+        _factory = factory.WithWebHostBuilder(builder => {
+            builder.ConfigureServices(services => {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                if (descriptor != null) {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<ApplicationDbContext>(options => {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+            });
+        });
+
+        _client = _factory.CreateClient();
+    }
+}
